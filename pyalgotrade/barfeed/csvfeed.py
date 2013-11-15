889b8a10c9383c51fef87ebaee5113101625e22d
Tibor Kiss <tibor.kiss@gmail.com>
2013-11-15 20:01:41 +0100

    Limit date parser cache size

--- pyalgotrade/barfeed/csvfeed.py
+++ pyalgotrade/barfeed/csvfeed.py
@@ -165,7 +165,7 @@ class BarFeed(membf.Feed):
 # Date,Open,High,Low,Close,Volume,Adj Close
 #
 # The csv Date column must have the following format: YYYY-MM-DD
-@memoize
+@memoize(maxlen=2048)  # Approx. 8 years worth of dates
 def parse_date(date):
 	# Sample: 2005-12-30
 	# This custom parsing works faster than:
